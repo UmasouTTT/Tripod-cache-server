4a4f8f44f8a24768320be02af971b26ac135bc66
yulai <yulai@node8.yulai-109767.ccjs-pg0.utah.cloudlab.us>
2021-11-04 20:05:00 -0600

    Liujiazhen

--- tripodCacheServer/cacheServer.py
+++ tripodCacheServer/cacheServer.py
@@ -1,5 +1,5 @@
 import math
-
+from time import *
 import boto3
 import fcntl
 from time import *
@@ -48,8 +48,8 @@ def load_file(file_path):
     f.close()
 
 def load_partial_object(key, range_start, end, stage_id, partition_id):
-    print("Start access object: {} _ {}".format(key, range))
-    for i in range(math.ceil((end - start)/piece_of_file)):
+    print("Start access object: {} _ {}-{}".format(key, range_start, end))
+    for i in range(math.ceil((end - range_start)/piece_of_file)):
         _start = range_start + i * piece_of_file
         _end = min(_start + piece_of_file, end)
         bytes_range = 'bytes=' + str(_start) + "-" + str(_end)
@@ -67,6 +67,7 @@ def start(prefetch_files, num_of_stages):
         end = prefetch_plan[3]
         partition_id = prefetch_plan[4]
         load_partial_object(file_name, start, end, stage_id, partition_id)
+        load_file("/proj/ccjs-PG0/tpch-spark/TripodConfig/stagesCachedCondition.txt")
 
 def get_prefetch_files(file_path):
     f = open(file_path, "r+", encoding="utf8")
@@ -87,8 +88,11 @@ if Query_type == 1:
     # # prefetch_files: [[filename, stage_id, start, end, partition_id]...]
     num_of_stages = 4
     file_path = "../generate_simulate_data/datas/1/partition_inf"
+    start_time = time()
     prefetch_files = get_prefetch_files(file_path)
     start(prefetch_files, 4)
+    end_time = time()
+    print(end_time - start_time)
     print(stage_cached_partition)
 
 
